4da0578d30857e7583fe8ab597028b6fe12de247
Timothy Millward <silhouettetechnician@gmail.com>
2019-08-29 16:31:06 +0100

    integrating banner

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 const Error = ( props ) => {
     return ( 
-        <div>Hello from error</div>
+        <Hero>
+            <Banner className='banner' title='404' subtitle='page not found'>
+                <Link to='/' className='btn-primary'>Return Home</Link>
+            </Banner>
+        </Hero>
      );
 }
  
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom' 
+
 const Home = ( props ) => {
     return ( 
-        <Hero />
+        <Hero>
+            <Banner title='luxurious rooms' subtitle='deluxe rooms starting at £299'>
+                <Link to='/rooms' className='btn-primary'>Our Rooms</Link>
+            </Banner>
+        </Hero>
      );
 }
  
-export default Home;
+export default Home; 
